34b154c51e418fb84a6efd24ad77913c10070345
Lassi Kortela <lassi@lassi.io>
2019-06-23 12:16:23 +0300

    Test our output against GNU and BSD tar

--- test-chibi.scm
+++ test-chibi.scm
@@ -1,7 +1,27 @@
 #! /usr/bin/env chibi-scheme
 
 (import (scheme base)
+        (scheme file)
+        (scheme process-context)
+        (scheme read)
         (scheme write)
         (trivial-tar-writer))
 
-(tar-write-file "hello/world.text" (string->utf8 "hello world"))
+(define (slurp-binary-file filename)
+  (call-with-port
+   (open-binary-input-file filename)
+   (lambda (port)
+     (let loop ((whole (make-bytevector 0)))
+       (let ((part (read-bytevector 4096 port)))
+         (if (eof-object? part)
+             whole
+             (loop (bytevector-append whole part))))))))
+
+(tar-write-file "hello/world.text"
+                (slurp-binary-file "trivial-tar-writer.scm"))
+
+(define (main arguments)
+  (for-each (lambda (file) (tar-write-file file (slurp-binary-file file)))
+            (cdr arguments)))
+
+(main (command-line))
